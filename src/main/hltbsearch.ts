0a8ebf4e2d485463f87bea129d2747906e048004
Alex Katopodis <akatopo@users.noreply.github.com>
2024-11-15 21:16:06 +0200

    fetch search token for search endpoint

--- src/main/hltbsearch.ts
+++ src/main/hltbsearch.ts
@@ -70,7 +70,48 @@ export class HltbSearch {
     }
   }
 
+  // This is a terrible hack that's bound to break, let's see how long it lasts
+  async getSearchToken() {
+    const headers = {
+      'User-Agent': new UserAgent().toString(),
+      'origin': 'https://howlongtobeat.com/',
+      'referer': 'https://howlongtobeat.com/'
+    };
+
+    try {
+      const { text: pageSource } = await requestUrl({
+        method: 'GET',
+        headers,
+        url: HltbSearch.BASE_URL,
+      });
+
+      const [appUrl] = pageSource.match('_next/static/chunks/pages/_app-[0-9a-f]{16}.js');
+      if (!appUrl) {
+        throw new Error('Error in matching the app url from the page source');
+      }
+
+      const { text: appSource } = await requestUrl({
+        method: 'GET',
+        headers,
+        url: `${HltbSearch.BASE_URL}${appUrl}`,
+      });
+
+      const [ ,token1, token2] = appSource.match(
+        /fetch\("\/api\/search\/"\.concat\("([0-9a-f]+)"\)\.concat\("([0-9a-f]+)"\)/
+      );
+
+      if (!token1 || !token2) {
+        throw new Error('Error in matching the search token from the app source');
+      }
+
+      return `${token1}${token2}`
+    } catch (error) {
+      throw new Error(`Error in fetching the search token${error.message ? `: ${error.message}` : ''}`);
+    }
+  }
+
   async search(query: Array<string>, signal?: AbortSignal): Promise<any> {
+    const searchToken = await this.getSearchToken();
     // Use built-in javascript URLSearchParams as a drop-in replacement to create axios.post required data param
     let search = { ...this.payload };
     search.searchTerms = query;
@@ -83,7 +124,7 @@ export class HltbSearch {
           'origin': 'https://howlongtobeat.com/',
           'referer': 'https://howlongtobeat.com/'
         },
-        url: HltbSearch.SEARCH_URL,
+        url: `${HltbSearch.SEARCH_URL}/${searchToken}`,
         body: JSON.stringify(search),
       });
       return result;
